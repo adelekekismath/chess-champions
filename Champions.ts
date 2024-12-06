1b32b5dc1099a287039a42a0f4729d50eec71f56
adelekekismath9 <adelekekismath9@gmail.com>
2024-12-06 21:35:18 +0100

    refactor: optimize findChampions logic with 0(n) complexity

--- Champions.ts
+++ Champions.ts
@@ -1,62 +1,35 @@
 import { Player } from "./models/Types";
 
 
-function sortPlayers(players: Player[]): Player[] {
-  const playersWithoutDuplicates = Array.from(new Set(players));
-  return playersWithoutDuplicates.sort((a, b) => a.age - b.age || b.eloRating - a.eloRating);
-}
-
-function isEliminatedByChampion(champions: Player[], player: Player): boolean {
-  let start = 0;
-  let end = champions.length - 1;
-
-  while (start <= end) {
-    const middle = Math.floor((start + end) / 2);
-    const champion = champions[middle];
-
-    if ((champion.eloRating > player.eloRating && champion.age <= player.age) ||
-      (champion.age < player.age && champion.eloRating >= player.eloRating)) {
-      return true;
-    }
-
-    if (champion.age < player.age || (champion.age === player.age && champion.eloRating <= player.eloRating)) {
-      start = middle + 1;
-    } else {
-      end = middle - 1;
-    }
-  }
-
-  return false;
-}
-
 function findChampions(players: Player[]): Player[] {
 
   if (players.length < 2) return players;
 
-  const sortedPlayers = sortPlayers(players);
+
   const championsList: Player[] = [];
   const maxEloRatingByAge: number[] = [];
 
-  for (const player of sortedPlayers) {
-    const maxEloOfYoungerPlayers =
-      maxEloRatingByAge[player.age - 1] || -Infinity;
+  for (const player of players) 
+    maxEloRatingByAge[player.age] = Math.max(maxEloRatingByAge[player.age] || -1, player.eloRating);
+ 
+  for (const player of players){
+    let isChampion = true;
 
-    const isChampion =
-      maxEloOfYoungerPlayers <= player.eloRating &&
-      (!championsList.length || !isEliminatedByChampion(championsList, player));
+    if(player.eloRating  === maxEloRatingByAge[player.age] ){
+      for(let i=0; i < player.age; i++ ){
+        if(player.eloRating <= maxEloRatingByAge[i]){
+          isChampion = false;
+          break;
+        }    
+      }
 
-    if (isChampion)
-      championsList.push(player);
-
-    maxEloRatingByAge[player.age] = Math.max(
-      maxEloRatingByAge[player.age] || -Infinity,
-      player.eloRating
-    );
-
+      if(isChampion)
+        championsList.push(player);
+    }
   }
+
   return championsList;
 }
-
 
 
 export default findChampions;
